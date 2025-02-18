4e5de65ad9892a31a96f9ae27b5091223b6a6b53
jatingaur18 <jatin22101@iiitnr.edu.in>
2025-02-18 08:28:24 +0530

    attack bitboards

--- board.cpp
+++ board.cpp
@@ -1,39 +1,63 @@
-#include <bits/stdc++.h>
-#include <cstdint>
 #include "board.h"
+#include "utils.h"
+#include <iostream>
 using namespace std;
 
-// initialize the bitboard to 0
-chessboard::chessboard(){
-  bitboard = 0ULL;
+chessboard::chessboard() : bitboard(0ULL) {
+    init_attack_tables();
 }
 
-// print the board
-void chessboard::printBoard(){
-  cout<<endl;
-  for(int i = 0;i<8;i++){
-    for(int j = 0;j<8;j++){
-      if(j==0)cout<< 8-i <<"  ";
-      cout<<" "<<getBit(i*8+j);
+void chessboard::printBoard(const usl &board) const {
+    std::cout << "\n";
+    for (int rank = 0; rank < 8; rank++) {
+        std::cout << 8 - rank << "  ";
+        for (int file = 0; file < 8; file++) {
+            int square = rank * 8 + file;
+            std::cout << " " << getBit(square, board);
+        }
+        std::cout << "\n";
     }
-    cout<<endl;
-  }
-  cout<<endl;
-  cout<<"    a b c d e f g h"<<endl;
-
-  cout<<"\n Bitboard: "<< bitboard<<"\n"<<endl;
-}
-// set a bit in the bitboard
-void chessboard::setBit(int square){
-  bitboard|= (1ULL<<square);
+    std::cout << "\n    a b c d e f g h\n\n";
+    std::cout << "Bitboard: " << board << "\n";
 }
 
-// get a bit from the bitboard
-int chessboard::getBit(int square){
-  return (bitboard & (1ULL << square))?1:0;
+usl chessboard::pawn_attacks(Color color, int square) const {
+    usl pawn = 1ULL << square;
+    if (color) {
+        return ((pawn << 7) & FILE_H) | ((pawn << 9) & FILE_A);
+    } else {
+        return ((pawn >> 9) & FILE_H) | ((pawn >> 7) & FILE_A);
+    }
 }
 
-// remove a bit from the bitboard
-void chessboard::remBit(int square){
-  bitboard &= ~(1ULL<<square);
+usl chessboard::knight_attacks(int square) const {
+    usl knight = 1ULL << square;
+    usl attacks = ((knight & FILE_AB) << 6) | ((knight & FILE_A) << 15) |
+                  ((knight & FILE_H) << 17) | ((knight & FILE_GH) << 10) |
+                  ((knight & FILE_GH) >> 6) | ((knight & FILE_H) >> 15) |
+                  ((knight & FILE_A) >> 17) | ((knight & FILE_AB) >> 10);
+    return attacks;
+}
+
+usl chessboard::king_attacks(int square) const {
+  usl king = 1ULL << square;
+    return ((king << 8)|(king >> 8)|((king << 1) & FILE_A)|((king >> 1) & FILE_H)
+            |((king << 9) & FILE_A)|((king << 7) & FILE_H)|((king >> 7) & FILE_A)
+            |((king >> 9) & FILE_H));
+}
+
+usl chessboard::rook_attacks(int square, usl occupancy) const {
+    return (~(MSB(occupancy & rook_map[square][0])-1)& rook_map[square][0]
+    | ((LSB(occupancy & rook_map[square][1])<<1)-1)& rook_map[square][1]
+    | ~(MSB(occupancy & rook_map[square][2])-1)& rook_map[square][2]
+    | ((LSB(occupancy & rook_map[square][3])<<1)-1)& rook_map[square][3]);
+}
+
+void chessboard::init_attack_tables() {
+    for (int sq = 0; sq < 64; sq++) {
+        pawn_attacks_table[WHITE][sq] = pawn_attacks(WHITE, sq);
+        pawn_attacks_table[BLACK][sq] = pawn_attacks(BLACK, sq);
+        knight_attacks_table[sq] = knight_attacks(sq);
+        // king_attacks_table[sq] = king_attacks(sq); // if needed
+    }
 }
--- board.h
+++ board.h
@@ -1,40 +1,59 @@
-#ifndef BOARD_H
-#include <bits/stdc++.h>
+#pragma once
 #include <cstdint>
-using namespace std;
+#include <iostream>
 
-
-#define BOARD_H
 #define usl unsigned long long
 
-enum {
-  a8, b8, c8, d8, e8, f8, g8, h8, 
-  a7, b7, c7, d7, e7, f7, g7, h7, 
-  a6, b6, c6, d6, e6, f6, g6, h6, 
-  a5, b5, c5, d5, e5, f5, g5, h5, 
-  a4, b4, c4, d4, e4, f4, g4, h4, 
-  a3, b3, c3, d3, e3, f3, g3, h3, 
-  a2, b2, c2, d2, e2, f2, g2, h2, 
-  a1, b1, c1, d1, e1, f1, g1, h1
-};
-/*
-"a8", "b8", "c8", "d8", "e8", "f8", "g8", "h8", 
-"a7", "b7", "c7", "d7", "e7", "f7", "g7", "h7", 
-"a6", "b6", "c6", "d6", "e6", "f6", "g6", "h6", 
-"a5", "b5", "c5", "d5", "e5", "f5", "g5", "h5", 
-"a4", "b4", "c4", "d4", "e4", "f4", "g4", "h4", 
-"a3", "b3", "c3", "d3", "e3", "f3", "g3", "h3", 
-"a2", "b2", "c2", "d2", "e2", "f2", "g2", "h2", 
-"a1", "b1", "c1", "d1", "e1", "f1", "g1", "h1", 
-*/
+// const files
+constexpr usl FILE_A = 18374403900871474942ULL;
+constexpr usl FILE_H = 9187201950435737471ULL;
+constexpr usl FILE_GH = 4557430888798830399ULL;
+constexpr usl FILE_AB = 18229723555195321596ULL;
 
-class chessboard{
-  public:
-  usl bitboard;
-  chessboard();
-  void printBoard();
-  void setBit(int square);
-  int  getBit(int square);
-  void remBit(int square);
+enum Square {
+    a8, b8, c8, d8, e8, f8, g8, h8,
+    a7, b7, c7, d7, e7, f7, g7, h7,
+    a6, b6, c6, d6, e6, f6, g6, h6,
+    a5, b5, c5, d5, e5, f5, g5, h5,
+    a4, b4, c4, d4, e4, f4, g4, h4,
+    a3, b3, c3, d3, e3, f3, g3, h3,
+    a2, b2, c2, d2, e2, f2, g2, h2,
+    a1, b1, c1, d1, e1, f1, g1, h1
 };
-#endif
+
+enum Color { WHITE, BLACK };
+
+class chessboard {
+public:
+    usl bitboard;
+    usl pawn_attacks_table[2][64];
+    usl knight_attacks_table[64];
+
+    chessboard();
+
+    inline void setBit(int square, usl &board) {
+        board |= (1ULL << square);
+    }
+    inline int getBit(int square, const usl &board) const {
+        return (board & (1ULL << square)) ? 1 : 0;
+    }
+    inline void remBit(int square, usl &board) {
+        board &= ~(1ULL << square);
+    }
+    inline void setBit(int square) { setBit(square, bitboard); }
+    inline int getBit(int square) const { return getBit(square, bitboard); }
+    inline void remBit(int square) { remBit(square, bitboard); }
+
+    void printBoard(const usl &board) const;
+    void printBoard() const { printBoard(bitboard); }
+
+    usl pawn_attacks(Color color, int square) const;
+    usl knight_attacks(int square) const;
+    usl king_attacks(int square) const;
+    usl rook_attacks(int square, usl occupancy) const;
+
+    void init_attack_tables();
+};
+
+inline usl LSB(usl x) { return x & -x; }
+inline usl MSB(usl x) { return 1ULL << (63 - __builtin_clzll(x)); }
